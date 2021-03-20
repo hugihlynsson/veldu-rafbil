9f1b52e47e51a80ba1b7d19f88f3d0765f4500e3
Hugi Hlynsson <hugihlynsson@gmail.com>
2021-03-20 14:41:24 +0000

    Add a section with a link to orflaedi.is just above the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,60 +1,64 @@
 import { FunctionComponent } from 'react'
+import Orflaedi from './Orflaedi'
 
 const Footer: FunctionComponent<{}> = () => (
-  <footer>
-    <p>
-      Veldu Rafbíl er smíðuð af{' '}
-      <a href="https://hugihlynsson.com">Huga Hlynssyni</a> og er geymd á{' '}
-      <a href="https://github.com/hugihlynsson/veldu-rafbil">GitHub</a>.{' '}
-    </p>
+  <>
+    <Orflaedi />
+    <footer>
+      <p className="footer-text">
+        Veldu Rafbíl er smíðuð af{' '}
+        <a href="https://hugihlynsson.com">Huga Hlynssyni</a> og er geymd á{' '}
+        <a href="https://github.com/hugihlynsson/veldu-rafbil">GitHub</a>.{' '}
+      </p>
 
-    <p>
-      Ef þú ert með ábendingu eða fyrirspurn geturu sent póst á{' '}
-      <a href="mailto:hugi@hey.com">hugi@hey.com</a>.
-    </p>
+      <p className="footer-text">
+        Ef þú ert með ábendingu eða fyrirspurn geturu sent póst á{' '}
+        <a href="mailto:hugi@hey.com">hugi@hey.com</a>.
+      </p>
 
-    <style jsx>{`
-      footer {
-        background-color: #f8f8f8;
-        padding: 32px 0;
-      }
-      p {
-        margin: 0 auto;
-        max-width: 480px;
-        font-size: 14px;
-        line-height: 1.5;
-        font-weight: 300;
-        padding: 0 16px;
-      }
-      a {
-        color: #000;
-        font-weight: 500;
-        text-decoration: none;
-      }
-      a:hover {
-        text-decoration: underline;
-      }
-
-      @media screen and (min-width: 375px) {
+      <style jsx>{`
         footer {
-          padding: 56px 0;
+          background-color: #f8f8f8;
+          padding: 32px 0;
         }
         p {
-          padding: 0 24px;
+          margin: 0 auto;
+          max-width: 480px;
+          font-size: 14px;
+          line-height: 1.5;
+          font-weight: 300;
+          padding: 0 16px;
         }
-      }
+        a {
+          color: #000;
+          font-weight: 500;
+          text-decoration: none;
+        }
+        a:hover {
+          text-decoration: underline;
+        }
 
-      @media screen and (min-width: 768px) {
-        footer {
-          padding: 56px 0;
+        @media screen and (min-width: 375px) {
+          footer {
+            padding: 56px 0;
+          }
+          p {
+            padding: 0 24px;
+          }
         }
-        p {
-          max-width: 1024px;
-          padding: 0 40px;
+
+        @media screen and (min-width: 768px) {
+          footer {
+            padding: 56px 0;
+          }
+          p {
+            max-width: 1024px;
+            padding: 0 40px;
+          }
         }
-      }
-    `}</style>
-  </footer>
+      `}</style>
+    </footer>
+  </>
 )
 
 export default Footer
---
+++ components/Orflaedi.tsx
@@ -0,0 +1,116 @@
+import { FunctionComponent } from 'react'
+
+const Orflaedi: FunctionComponent<{}> = () => (
+  <section>
+    <div className="content">
+      <div className="texts">
+        <h1>Ertu að leita að enn hagkvæmara ökutæki?</h1>
+
+        <p>
+          <a href="https://www.orflaedi.is">Örflæði.is</a> er einfalt yfirlit
+          yfir öll rafhjól og örflæðistæki sem eru í boði á Íslandi. Þar hefur Jökull
+          Sólberg, áhugamaður um samgöngur, flokkað öll tækin eftir því hvar þau eru í
+          umferðarlögunum, klippt til myndir og tekið saman helstu
+          tækniupplýsingarnar.
+        </p>
+      </div>
+
+      <a href="https://www.orflaedi.is" className="card">
+        Öll létt rafknúin ökutæki á einum stað
+        <img src="/orflaedi-logo.svg" />
+      </a>
+    </div>
+
+    <style jsx>{`
+      section {
+        background-color: #e2e8f0;
+        margin-bottom: 2px;
+      }
+      .content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 0 auto;
+        padding: 32px 16px;
+      }
+      .texts {
+        margin-bottom: 24px;
+      }
+      h1 {
+        margin: 0 0 20px 0;
+        font-weight: 900;
+        font-size: 32px;
+        max-width: 480px;
+        line-height: 1.1;
+        color: #1a202c;
+      }
+      p {
+        margin: 0;
+        max-width: 480px;
+        font-size: 14px;
+        line-height: 1.5;
+        color: #4a5568;
+      }
+      p a {
+        color: #000;
+        font-weight: 500;
+        text-decoration: none;
+      }
+
+      p a:hover {
+        text-decoration: underline;
+      }
+      .card {
+        flex-shrink: 0;
+        display: block;
+        max-width: 320px;
+        background-color: #1a202c;
+        border-radius: 16px;
+        padding: 24px;
+        padding-right: 64px;
+        box-shadow: 0 2px 32px rgba(0, 0, 0, 0.2);
+        transition: all 0.2s;
+        color: white;
+        path: white !important;
+        text-decoration: none;
+        font-size: 20px;
+        line-height: 1.5;
+        font-weight: 600;
+      }
+      .card:hover {
+        box-shadow: 0 8px 64px rgba(0, 0, 0, 0.3);
+        transform: translateY(-4px);
+      }
+      .card img {
+        display: block;
+        height: auto;
+        max-width: 100%;
+        height: 30px;
+        margin-top: 32px;
+      }
+
+      @media screen and (min-width: 375px) {
+        .content {
+          max-width: 480px;
+          padding: 56px 24px;
+        }
+      }
+
+      @media screen and (min-width: 768px) {
+        .content {
+          padding: 56px 40px;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: center;
+          max-width: 1024px;
+        }
+        .texts {
+          margin-bottom: 0;
+          margin-right: 32px;
+        }
+      }
+    `}</style>
+  </section>
+)
+
+export default Orflaedi
