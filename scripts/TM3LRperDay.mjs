3b45e4fae9a03745deb0295c4827d1534af9c9db
Hugi Hlynsson <hugihlynsson@gmail.com>
2021-09-06 08:22:56 +0000

    Add a script to calculate the amount of used TM3LR on sale every day

---
+++ scripts/TM3LRperDay.mjs
@@ -0,0 +1,33 @@
+// Export Firebase Database and store it at the root of this project
+// Run node scripts/TM3LRperDay.mjs to get some results ✨
+
+import { readFile } from 'fs/promises'
+
+const data = JSON.parse(
+  await readFile(new URL('./choose-ev-export.json', import.meta.url)),
+)
+
+const formatDate = (date) =>
+  `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
+
+const dates = Object.values(data.snapshots).map((snapshot) =>
+  formatDate(new Date(snapshot.timestamp)),
+)
+const uniqueDates = Array.from(new Set(dates))
+const uniqueDateSnapshots = uniqueDates.map((date) =>
+  Object.values(data.snapshots).find(
+    (snapshot) => date === formatDate(new Date(snapshot.timestamp)),
+  ),
+)
+
+const counts = uniqueDateSnapshots.map((snapshot) => {
+  const tm3lrs = snapshot.cars.filter((car) =>
+    Boolean(car.metadata?.id == 'tesla-model-3-long-range'),
+  )
+
+  const date = formatDate(new Date(snapshot.timestamp))
+  return `${date}\t${tm3lrs.length}`
+})
+
+console.log(counts.join('\n '))
+// console.log(JSON.stringify(counts, null, 2))
